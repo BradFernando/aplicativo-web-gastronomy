7706e1a017599aabf644891449aa0a374ad336e4
BradFernando <bfcorro@espe.edu.ec>
2024-04-26 17:56:46 -0500

    Second Commit

--- pages/productCard.tsx
+++ pages/productCard.tsx
@@ -16,47 +16,49 @@ interface ProductCardProps {
     image: string;
   };
 }
-
 export default function ProductCard({ form }: ProductCardProps) {
-  return (
-    <Card sx={{
-        backgroundColor: '#222222',
-        color: 'white',
-        margin: 0,
-        padding: 0,
-        width: '100%'
-    }}>
-      <CardContent sx={{ width: '100%' }}>
-        <Box sx={{ maxWidth: '100%', width: '100%', overflowWrap: 'break-word' }}>
-          <Typography variant="h5" component="div">
-            {form.name}
-          </Typography>
-          <Typography variant="subtitle1" color="text.secondary">
-            {form.category}
-          </Typography>
-          <Typography variant="body2" sx={{ overflowY: 'auto', maxHeight: 100, wordWrap: 'break-word', whiteSpace: 'pre-wrap' }}>
-            {form.description}
-          </Typography>
-            <Box sx={{ display: 'flex', gap: 18}}>
-              <Typography variant="body2" sx={{ overflowY: 'auto', maxHeight: 25, wordWrap: 'break-word', whiteSpace: 'pre-wrap' }}>
-                  Cantidad: {form.cantidad}
+    if (!form) {
+        return null; // O puedes renderizar algún componente de carga o de error aquí
+    }
+      return (
+        <Card sx={{
+            backgroundColor: '#222222',
+            color: 'white',
+            margin: 0,
+            padding: 0,
+            width: '100%'
+        }}>
+          <CardContent sx={{ width: '100%' }}>
+            <Box sx={{ maxWidth: '100%', width: '100%', overflowWrap: 'break-word' }}>
+              <Typography variant="h5" component="div">
+                {form.name}
               </Typography>
-              <Typography variant="body2" sx={{ overflowY: 'auto', maxHeight: 25, wordWrap: 'break-word', whiteSpace: 'pre-wrap' }}>
-                  Precio: {form.precio}
+              <Typography variant="subtitle1" color="text.secondary">
+                {form.category}
               </Typography>
-          </Box>
-        </Box>
-        {form.image && (
-            <Box sx={{ width: '100%', height: '170px', position: 'relative', right: 18 }}>
-                <Image
-                    src={`data:image/png;base64,${form.image}`}
-                    alt={form.name}
-                    layout="fill"
-                    objectFit="contain"
-                />
+              <Typography variant="body2" sx={{ overflowY: 'auto', maxHeight: 100, wordWrap: 'break-word', whiteSpace: 'pre-wrap' }}>
+                {form.description}
+              </Typography>
+                <Box sx={{ display: 'flex', gap: 18}}>
+                  <Typography variant="body2" sx={{ overflowY: 'auto', maxHeight: 25, wordWrap: 'break-word', whiteSpace: 'pre-wrap' }}>
+                      Cantidad: {form.cantidad}
+                  </Typography>
+                  <Typography variant="body2" sx={{ overflowY: 'auto', maxHeight: 25, wordWrap: 'break-word', whiteSpace: 'pre-wrap' }}>
+                      Precio: {form.precio}
+                  </Typography>
+              </Box>
             </Box>
-        )}
-      </CardContent>
-    </Card>
-  );
+            {form.image && (
+                <Box sx={{ width: '100%', height: '170px', position: 'relative', right: 18 }}>
+                    <Image
+                        src={`data:image/png;base64,${form.image}`}
+                        alt={form.name}
+                        layout="fill"
+                        objectFit="contain"
+                    />
+                </Box>
+            )}
+          </CardContent>
+        </Card>
+      );
 }
